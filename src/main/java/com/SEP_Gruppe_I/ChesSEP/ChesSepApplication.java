ff0817cdfdfe77dc1db22ba9225858d4068b5d74
“Jonas <jonas.giesen@stud.uni-duisburg-essen.de>
2023-10-20 15:03:48 +0200

    Springboot init

---
+++ src/main/java/com/SEP_Gruppe_I/ChesSEP/ChesSepApplication.java
@@ -0,0 +1,13 @@
+package com.SEP_Gruppe_I.ChesSEP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChesSepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChesSepApplication.class, args);
+	}
+
+}
