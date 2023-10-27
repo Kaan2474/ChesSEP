1729446412d6ae9b640b073fce0897e5c6dc6918
Seher <seher.tezer@stud.uni-due.de>
2023-10-27 14:58:43 +0200

    Frontend

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,20 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PreLoginComponent } from './pre-login/pre-login.component';
+
 import { LoginComponent } from './PreLogin/login/login.component';
-import { StartseiteComponent } from './PostLogin/startseite/startseite.component';
+
 import { HomepageComponent } from './PostLogin/homepage/homepage.component';
-import { RegistrierungComponent } from './PreLogin/registrierung/registrierung.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PreLoginComponent,
+
     LoginComponent,
-    StartseiteComponent,
+
     HomepageComponent,
-    RegistrierungComponent
+
   ],
   imports: [
     BrowserModule
