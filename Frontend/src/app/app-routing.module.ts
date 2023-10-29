8a19fe922b78dc3125a35a9de5b9acb4d68ece60
Seher <seher.tezer@stud.uni-due.de>
2023-10-29 11:13:12 +0100

    Routing Modul ergänzt

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from "./PreLogin/login/login.component";
 import {HomepageComponent} from "./PostLogin/homepage/homepage.component";
+import {RegisterComponent} from "./PreLogin/register/register.component";
 
 const routes: Routes = [
   {path: "login", title:"Login", component: LoginComponent},
-  {path: "", title:"HomePage", component: HomepageComponent}
+  {path: "", title:"HomePage", component: HomepageComponent},
+  {path: "register", title:"Register", component: RegisterComponent}
 ];
 
 
