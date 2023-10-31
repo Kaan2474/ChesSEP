bf751da4455d1461428928ef382b2dad5ab8f067
Kaan Hisiroglu <snkahisi@uni-duisburg-essen.de>
2023-10-31 14:21:13 +0100

    Postlogin Header add friend

---
+++ Frontend/src/app/PostLogin/addfriend/addfriend.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-addfriend',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './addfriend.component.html',
+  styleUrls: ['./addfriend.component.css']
+})
+export class AddfriendComponent {
+
+}
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,23 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { PreLoginComponent } from './pre-login/pre-login.component';
 import { LoginComponent } from './PreLogin/login/login.component';
-import { StartseiteComponent } from './PostLogin/startseite/startseite.component';
 import { HomepageComponent } from './PostLogin/homepage/homepage.component';
-import { RegistrierungComponent } from './PreLogin/registrierung/registrierung.component';
+import { HeaderComponent } from "./PostLogin/header/header.component";
+import { AddfriendComponent } from "./PostLogin/addfriend/addfriend.component";
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PreLoginComponent,
     LoginComponent,
-    StartseiteComponent,
     HomepageComponent,
-    RegistrierungComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeaderComponent,
+    AddfriendComponent
+
   ],
   providers: [],
   bootstrap: [AppComponent]
