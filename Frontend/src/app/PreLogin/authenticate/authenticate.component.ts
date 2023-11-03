a768b96a4aaabfde66fcda5c12b3504a479cbc66
Ayoub Ouamar <ayoub.ouamar@stud.uni-due.de>
2023-11-03 22:44:21 +0100

    gitignore Zeile 23-25 hinzugefügt

--- Frontend/src/app/PreLogin/authenticate/authenticate.component.ts
+++ Frontend/src/app/PreLogin/authenticate/authenticate.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {User} from "../../Modules/User";
+import {UserService} from "../../Service/user.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-authenticate',
@@ -6,5 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./authenticate.component.css']
 })
 export class AuthenticateComponent{
+  user:User;
+  code:String="";
 
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private userService: UserService) {this.user = new User()}
+
+  submitCode(){
+    this.user.email = localStorage.getItem("ActiveUser");
+    this.user.verificationCode = this.code;
+    this.userService.checkCode(this.user).subscribe((res) =>
+      {this.goToHomePage()},
+      (error) => {
+        this.errorWithForm()
+      })
+  }
+  goToHomePage(){
+    this.router.navigate(["/homepage"]);
+  }
+  errorWithForm(){
+    alert("Ungültige Eingabe! Überprüfe deine Angabe!");
+  }
 }
