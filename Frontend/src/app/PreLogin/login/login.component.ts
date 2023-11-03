e63b464846359a22a1a330a38dbc7bce91e25be7
Ayoub Ouamar <ayoub.ouamar@stud.uni-due.de>
2023-11-03 22:21:32 +0100

    HomePage Reihenfolge geändert

--- Frontend/src/app/PreLogin/login/login.component.ts
+++ Frontend/src/app/PreLogin/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {User} from "../../Modules/User";
+import {UserService} from "../../Service/user.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 
 @Component({
@@ -8,5 +11,27 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  user:User;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private userService:UserService)
+              {this.user=new User()}
+
+
+  onSubmit()
+  {
+    this.userService.login(this.user).subscribe(result => {this.goToAuthentication()}, (error) => {this.errorWithForm();})
+  }
+
+  private errorWithForm()
+  {
+    alert("Ungültige Eingabe! Überprüfe deine Angabe!");
+  }
+
+  goToAuthentication(){
+    localStorage.setItem("ActiveUser", this.user.email);
+    this.router.navigate(["/Authentication"]);
+  }
 
 }
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './PreLogin/login/login.component';
@@ -44,6 +45,7 @@ import {JoingameComponent} from "./PostLogin/joingame/joingame.component";
     AppRoutingModule,
     RouterOutlet,
     FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
