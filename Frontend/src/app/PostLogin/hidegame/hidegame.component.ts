d25649480cb1997990e502fa36b035a57b2cbb6b
Ayoub Ouamar <ayoub.ouamar@stud.uni-due.de>
2023-11-11 14:03:22 +0100

    HideGame getestet, ob Daten mitgenommen werden

--- Frontend/src/app/PostLogin/hidegame/hidegame.component.ts
+++ Frontend/src/app/PostLogin/hidegame/hidegame.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-hidegame',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./hidegame.component.css']
 })
 export class HidegameComponent {
+  timer?: number; //Da Variable initialisiert werden muss => ?
+  difficulty?: string;
+  gameName?: string;
 
+  constructor(private route: ActivatedRoute) {
+    this.route.queryParams.subscribe((params) => {
+      this.timer = parseInt(params['timer']);
+      this.difficulty = params['difficulty'];
+    }
+    )
+    const storedGameName = localStorage.getItem("gameName");
+    if (storedGameName) {
+      this.gameName = storedGameName;
+    };
+  }
 }
+
