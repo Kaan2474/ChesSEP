ea3c4a1857d65002c4cfb416f8ba41b2d3a1da51
Ayoub Ouamar <ayoub.ouamar@stud.uni-due.de>
2023-12-07 11:45:52 +0100

    PrivateChat und Chat Service angefangen

---
+++ Frontend/src/app/Modules/GroupMessage.ts
@@ -0,0 +1,8 @@
+export class GroupMessage{
+  messageId:any;
+  senderId:any;
+  recipients:any;
+  content:any;
+  time: any;
+  read:any;
+}
--- Frontend/src/app/PostLogin/private-chat/private-chat.component.ts
+++ Frontend/src/app/PostLogin/private-chat/private-chat.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {User} from "../../Modules/User";
+import {UserService} from "../../Service/user.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {ChatService} from "../../Service/chat.service";
+import {Message} from "../../Modules/Message";
 
 @Component({
   selector: 'app-private-chat',
   templateUrl: './private-chat.component.html',
   styleUrls: ['./private-chat.component.css']
 })
-export class PrivateChatComponent {
+export class PrivateChatComponent implements OnInit,OnDestroy{
+  token = localStorage.getItem("JWT");
+  id: any;
+  messages:Message[]=[];
+  newMessage:Message;
+  content:any;
+  user:any;
+  friend:any;
+
+  constructor(private userService: UserService,
+              private chatService : ChatService,
+              private route: ActivatedRoute,
+              private router: Router) {
+
+              this.user = new User();
+              this.friend = new User();
+              this.newMessage = new Message();
+              }
+
+  ngOnInit() {
+    this.getUserDetail();
+    this.getFriendDetails();
+  }
+  ngOnDestroy(): void {
+  }
+  getUserDetail() {
+    this.userService.getUserbyToken().subscribe((data) => {
+      console.log(data)
+      this.user = data;
+
+    },);
+  }
+  getFriendDetails(){
+    this.id = this.route.snapshot.params["id"];
+    this.userService.getUser(this.id).subscribe((data) => {
+      console.log(data)
+      this.friend = data
+    },);
+  }
+
+
 
 }
