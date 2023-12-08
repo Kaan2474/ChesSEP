b4fa20d740977f29da2ba83210171c800980b01b
Ayoub Ouamar <ayoub.ouamar@stud.uni-due.de>
2023-12-08 15:08:33 +0100

    ChatService und Chat Klasse fertig

---
+++ Frontend/src/app/Modules/Chat.ts
@@ -0,0 +1,14 @@
+export class Chat {
+  ownerId:any;
+  recipientId:any;
+  chessClubId:any;
+  chessClubName:any;
+  groupName:any;
+  user?:any[];
+  content:any;
+  chatId:any;
+  chatMessageStatus:any;
+  senderId:any;
+  newContent:any;
+  oldContent:any;
+}
