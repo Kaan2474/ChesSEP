8a80db03667686c203318a1c3af29d9045355b8a
Mario Mario <mario.mai@stud.uni.due.de>
2023-12-10 13:46:57 +0100

    Quellcode aufgeräumt -> Keine Änderungen an der Logik

--- Backend/src/main/java/com/ChesSEP/ChesSEP/Chat/ChatController.java
+++ Backend/src/main/java/com/ChesSEP/ChesSEP/Chat/ChatController.java
@@ -18,6 +18,9 @@ public class ChatController {
         this.chatService = chatService;
     }
 
+    //####################Alles rund um Private Unterhaltung################################
+
+    //Erstelle eine Private Uneterhaltung
     @GetMapping("/createPrivateChat/{friendId}")
     public ResponseEntity<Boolean> createPrivateChat(@PathVariable long friendId) {
         boolean check = chatService.createPrivateChat(friendId);
@@ -28,6 +31,33 @@ public class ChatController {
         }
     }
 
+    /*Schreibe eine Message an die Unterhaltung ->chatId (Message an eine Private Unterhaltung)
+    Der Content wird aus dem Objekt ChatRequestDto entnommen -> String content
+     */
+    @PostMapping("/writeMessagePrivateChat/{chatId}")
+    public ResponseEntity<Boolean> writeMessage(@PathVariable long chatId, @RequestBody ChatRequestDto chatRequestDto){
+        Boolean check = chatService.writeMessage(chatRequestDto.getContent(), chatId);
+        if(check){
+            return new ResponseEntity<>(check, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(check, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    //Gibt einen Privaten Chat zurück, wo SenderId umd EmpfängerId übereinstimmen (auch umgekehrt)
+    @GetMapping("/getMyPrivateChatWith/{friendId}")
+    public ResponseEntity<Chat> getMyPrivateChatWith(@PathVariable long friendId){
+        Chat chat = chatService.getPrivateChatWith(friendId);
+        if(chat != null){
+            return new ResponseEntity<>(chat, HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(chat, HttpStatus.BAD_REQUEST); //wenn Chat == null
+        }
+    }
+
+    //####################Alles rund um Gruppen Unterhaltung################################
+
+    //Erstelle eine Gruppen Uneterhaltung mit mehreren Usern
     @GetMapping("/createGroupChat/{groupName}/{user}")
     public ResponseEntity<Boolean> createGroupChat(@PathVariable String groupName, @PathVariable List<Long> user){
 
@@ -41,18 +71,9 @@ public class ChatController {
         }
     }
 
-
-    //privateChat Message
-    @PostMapping("/writeMessagePrivateChat/{chatId}")
-    public ResponseEntity<Boolean> writeMessage(@PathVariable long chatId, @RequestBody ChatRequestDto chatRequestDto){
-        Boolean check = chatService.writeMessage(chatRequestDto.getContent(), chatId);
-        if(check){
-            return new ResponseEntity<>(check, HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>(check, HttpStatus.BAD_REQUEST);
-        }
-    }
-
+    /*Schreibe eine Message an die Gruppen Unterhaltung mit der angegebenen chatId
+    Der Content wird aus dem Objekt ChatRequestDto entnommen -> String content
+     */
     @PostMapping("/writeMessageGroup/{chatId}")
     public ResponseEntity<Boolean> writeMessageGroup(@PathVariable long chatId, @RequestBody ChatRequestDto chatRequestDto) {
         Boolean check = chatService.writeMessageGroup(chatRequestDto.getContent(), chatId);
@@ -63,56 +84,14 @@ public class ChatController {
         }
     }
 
-    //Gibt alle Members aus einer Gruppe zurück - klappt - für GruppenChat
+    //Gibt alle Members aus einer Gruppe Unterhaltung zurück
     @GetMapping("/members/{chatId}")
     public ResponseEntity<List<Long>> membersOfChatId(@PathVariable long chatId){
         return ResponseEntity.ok(chatService.memberOfChatId(chatId));
     }
 
-    //Gibt neuste Nachricht(en) zurück
-    @GetMapping("/latest/{chatId}/{time}")
-    public ResponseEntity<List<ChatMessage>> findLatestMessage(@PathVariable long chatId, @PathVariable long time){
-        return ResponseEntity.ok(chatService.getNewMessage(chatId, time));
-    }
 
-    //Gibt eine List aus, mit allen Nachrichten die ungelesen sind und somit verändert/gelöscht werden können
-    @GetMapping("/changableMessage/{chatId}")
-    public ResponseEntity<List<ChatMessage>> lastWrittenChangeableMessage(@PathVariable long chatId){
-        return ResponseEntity.ok(chatService.myChangeableMessage(chatId));
-    }
-
-    //Verändert eine Ungelesene Nachricht
-    @PostMapping("/changeMessage/{chatId}")
-    public ResponseEntity<Boolean> changeMessage(@PathVariable long chatId, @RequestBody ChatRequestDto chatRequestDto){
-        boolean check = chatService.setChangeMessage(chatId, chatRequestDto.getOldContent(), chatRequestDto.getNewContent());
-        if(check){
-            return new ResponseEntity<>(check, HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>(check, HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    //löscht eine ungelesene Nachricht
-    @PostMapping("/deleteMessage/{chatId}")
-    public ResponseEntity<Boolean> deleteMessage(@PathVariable long chatId, @RequestBody ChatRequestDto chatRequestDto){
-        boolean check = chatService.deleteMessage(chatId, chatRequestDto.getOldContent());
-        if(check){
-            return new ResponseEntity<>(check, HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>(check, HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    @GetMapping("/getMyPrivateChatWith/{friendId}")
-    public ResponseEntity<Chat> getMyPrivateChatWith(@PathVariable long friendId){
-        Chat chat = chatService.getPrivateChatWith(friendId);
-        if(chat != null){
-            return new ResponseEntity<>(chat, HttpStatus.OK);
-        }else {
-            return new ResponseEntity<>(chat, HttpStatus.BAD_REQUEST); //wenn Chat == null
-        }
-    }
-
+    //Gibt den Chat zurück, der mit der Gruppenbezeichnung übereinstimmt
     @GetMapping("/getGroupByGroupName/{privateGroupName}")
     public ResponseEntity<Chat> getGroupByGroupName(@PathVariable String privateGroupName){
         Chat chat = chatService.getGroupByGroupName(privateGroupName);
@@ -123,13 +102,67 @@ public class ChatController {
         }
     }
 
+    //Gibt alle Gruppen Unterhaltungen zurück, die dem Sender zugeordnet werden können
     @GetMapping("/allMyGroupChats")
     public List<Chat> findAllMyGroupChats(){ return chatService.findAllMyGroupChats();}
 
+
+
+
+    //#################Alles Rund um Nachrichten#######################
+
+
+    //Gibt neuste Nachricht(en) zurück
+    @GetMapping("/latest/{chatId}/{time}")
+    public ResponseEntity<List<ChatMessage>> findLatestMessage(@PathVariable long chatId, @PathVariable long time){
+        return ResponseEntity.ok(chatService.getNewMessage(chatId, time));
+    }
+
+
+    //Gibt eine List aus, mit allen Nachrichten die ungelesen sind und somit verändert/gelöscht werden können
+    @GetMapping("/changableMessage/{chatId}")
+    public ResponseEntity<List<ChatMessage>> lastWrittenChangeableMessage(@PathVariable long chatId){
+        return ResponseEntity.ok(chatService.myChangeableMessage(chatId));
+    }
+
+
+
+    /*Verändert den Content einer UNGELESENEN "UNREAD" Nachricht
+    Hier ist zu verstehen, dass getOldContent verglichen wird mit der Nachricht, die in der DB zu finden ist.
+    Bei einer übereinstimmung, wird diese zu getNewContent verändert
+     */
+    @PostMapping("/changeMessage/{chatId}")
+    public ResponseEntity<Boolean> changeMessage(@PathVariable long chatId, @RequestBody ChatRequestDto chatRequestDto){
+        boolean check = chatService.setChangeMessage(chatId, chatRequestDto.getOldContent(), chatRequestDto.getNewContent());
+        if(check){
+            return new ResponseEntity<>(check, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(check, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+    /*Löscht eine UNGELESENEN "UNREAD" Nachricht
+    Hier ist zu verstehen, dass getOldContent verglichen wird mit der Nachricht, die in der DB zu finden ist.
+    Bei einer übereinstimmung, wird diese gelöscht
+     */
+    @PostMapping("/deleteMessage/{chatId}")
+    public ResponseEntity<Boolean> deleteMessage(@PathVariable long chatId, @RequestBody ChatRequestDto chatRequestDto){
+        boolean check = chatService.deleteMessage(chatId, chatRequestDto.getOldContent());
+        if(check){
+            return new ResponseEntity<>(check, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(check, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    //Gibt die Nachrichten aus, die in einer Unterhaltung (Gruppe oder Private) sind
     @GetMapping("/getMessages/{chatId}")
     public ResponseEntity<List<ChatMessage>> messages (@PathVariable long chatId){
         return ResponseEntity.ok(chatService.findChatMessagesOf(chatId));
     }
+
+
 
 
     ///////////////Verworfene Methoden//////////////////////////////////
--- Backend/src/main/java/com/ChesSEP/ChesSEP/Chat/ChatService.java
+++ Backend/src/main/java/com/ChesSEP/ChesSEP/Chat/ChatService.java
@@ -33,10 +33,9 @@ public class ChatService {
         this.userRepository = userRepository;
     }
 
-    /*ChatRequestDtp mit Übergeben und dann dort die Id als ownerId setzen, ChatType wird gesetzt, jenachdem wie viele im Chat sind
-    wird nur eien Person eingeladen = privat
-    ab > 1 = Group
-     */
+
+
+    //###################Alles Rund um Private Unterhaltungen#######################
     public boolean createPrivateChat(Long friendId) {
 
         if (chatRepository.getPrivateChat(getSender().getId(), friendId) != null) {
@@ -51,6 +50,9 @@ public class ChatService {
             return true;
         }
     }
+
+
+    //###############Alles Rund um Gruppen Unterhaltungen###################
 
     //Gruppenchat Erstellung
     public boolean createGroupChat(List<Long> user, String privateGroupName) {
@@ -68,7 +70,7 @@ public class ChatService {
         return true;
     }
 
-    //Für das erstellen von ChessClubChats, welches beim erstellen eines Clubs direkt mit erstellt wird
+    //Für das Erstellen eines ChessClubChats, welcher beim erstellen eines Clubs direkt mit erstellt wird
     public void createChessClubChat(String name) {
 
         List<Long> member = new ArrayList<>();
@@ -87,9 +89,8 @@ public class ChatService {
     }
 
 
-    /*
-    Liste der ChatTeilnehmer wird um getSender erweitert
-     */
+    //Liste der ChatTeilnehmer wird um getSender erweitert
+
     public void updateChessClubChat(String clubName) {
         Chat a = chatRepository.findChatByClubName(clubName);
         List<User> member = userRepository.getChessClubMember(chessClubRepository.findChessClubByName(clubName).getId());
@@ -102,38 +103,8 @@ public class ChatService {
 
     }
 
-    public boolean leaveGroupChat(String privateGroupName) {
-        if (chatRepository.findChatByGroupName(privateGroupName) != null && chatRepository.findChatByGroupName(privateGroupName).getUser().contains(getSender().getId())) {
-            Chat leftGroup = chatRepository.findChatByGroupName(privateGroupName);
-            leftGroup.getUser().remove(getSender().getId());
-            chatRepository.save(leftGroup);
-            return true;
-        } else {
-            return false;
-        }
-    }
 
-    public boolean deleteGroupChat(String privateGroupName) {
-        Chat toDelete = chatRepository.findChatByGroupName(privateGroupName);
-        if (toDelete != null && Objects.equals(toDelete.getOwnerId(), getSender().getId())) {
-            chatRepository.delete(toDelete);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    //Delete Unterhaltung (Muss noch getestet werden, ob Recipient ebenfalls löschen kann)
-    public boolean deletePrivateChat(long recipientId) {
-        Chat toDelete = chatRepository.getPrivateChat(getSender().getId(), recipientId);
-        if (toDelete != null) {
-            chatRepository.delete(toDelete);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
+    //Gibt ein Chat Objekt zurück, welches mit dem privateGroupName übereinstimmt
     public Chat getGroupByGroupName(String privateGroupName){
         return chatRepository.findChatByGroupName(privateGroupName);
     }
@@ -150,6 +121,29 @@ public class ChatService {
             return true;
         }
     }
+
+    //Gibt private Chat Objekt aus
+    public Chat getPrivateChatWith(long friendId){
+        return chatRepository.getPrivateChat(getSender().getId(), friendId);
+    }
+
+    //Rückgabe von Member aus einem Gruppenchat
+    public List<Long> memberOfChatId(long chatId) {
+        List<Long> members;
+        members = chatRepository.findChatByChatId(chatId).getUser();
+        return members;
+    }
+
+    //Gitb alle Gruppenchats zurück, in denen ich Mitglied bin
+    public List<Chat> findAllMyGroupChats(){
+        return chatRepository.findAllGroupChatsOfUserId(getSender().getId());
+    }
+
+
+
+
+    //##################Alles Rund um Nachrichten###############################
+
 
     //Für das schreiben von Nachrichten in Privaten Unterhaltungen (1 zu 1)
     //findByChatIDAndUserId -> Buggy
@@ -181,6 +175,7 @@ public class ChatService {
         }
     }
 
+
     //Liste aller Nachrichten, nach newestMessageTime
     //Sobald jemand anderes die Nachricht abruft, wird der Status der Nachricht auf ChatMessageStatus.READ gesetzt
     public List<ChatMessage> getNewMessage(long chatId, long newestMessageTime) {
@@ -192,10 +187,10 @@ public class ChatService {
                 chatMessageRepository.save(x);
             }
         }
-
-
         return list;
     }
+
+
 
     //Lösche Chat aus DB -> Für Owner
     public void deleteChat(String clubName) {
@@ -204,28 +199,20 @@ public class ChatService {
         }
     }
 
-    //Gibt private Chat Objekt aus
-    public Chat getPrivateChatWith(long friendId){
-        return chatRepository.getPrivateChat(getSender().getId(), friendId);
-    }
+
 
     //Gibt Nachrichten aus chatId aus
     public List<ChatMessage> findChatMessagesOf(long chatId) {
         return chatMessageRepository.findChatMessagesOf(chatId);
     }
 
-    //Rückgabe von Member aus einem Gruppenchat
-    public List<Long> memberOfChatId(long chatId) {
-        List<Long> members;
-        members = chatRepository.findChatByChatId(chatId).getUser();
-        return members;
-    }
 
-
+    //Gibt all meine Nachrichten aus, die ich grschrieben haben
     private List<ChatMessage> myLastWrittenMessage(long chatId, long senderId) {
         return chatMessageRepository.myWrittenMessage(chatId, senderId);
     }
 
+    //Gibt eine Liste aus, mit den Nachrichten, die auf UNREAD stehen
     public List<ChatMessage> myChangeableMessage(long chatId) {
         List<ChatMessage> messages = myLastWrittenMessage(chatId, getSender().getId());
         List<ChatMessage> unreadMessage = new ArrayList<>();
@@ -240,6 +227,8 @@ public class ChatService {
         return unreadMessage;
     }
 
+
+    //Verändert eine Nachricht, die ich geschrieben habe
     public boolean setChangeMessage(long chatId, String oldContent, String newContent) {
         if(chatRepository.findByChatIdAndUserId(chatId, getSender().getId())==null){
             return false;
@@ -256,8 +245,7 @@ public class ChatService {
         return false;
     }
 
-
-
+    //Löscht eine von mir geschriebene Nachricht
     public boolean deleteMessage(long chatId, String oldContent){
         if(chatRepository.findByChatIdAndUserId(chatId, getSender().getId())==null){
             return false;
@@ -272,27 +260,44 @@ public class ChatService {
         return false;
     }
 
-    public List<Chat> findAllMyGroupChats(){
-        return chatRepository.findAllGroupChatsOfUserId(getSender().getId());
-    }
 
-
-}
-
-
-
-
-
-    /*
-    Es fehlen noch
-    -> Ändere/Lösche Message bevor jemand anderes die Nachricht gelesen hat
-    -> Gib alle meine ChatGruppen aus
-     */
 
 
 
     ////////////////////////Methoden die eig nicht mehr gebraucht werden/////////////////////////////////////////////////////////////////////////
 
+
+    public boolean leaveGroupChat(String privateGroupName) {
+        if (chatRepository.findChatByGroupName(privateGroupName) != null && chatRepository.findChatByGroupName(privateGroupName).getUser().contains(getSender().getId())) {
+            Chat leftGroup = chatRepository.findChatByGroupName(privateGroupName);
+            leftGroup.getUser().remove(getSender().getId());
+            chatRepository.save(leftGroup);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean deleteGroupChat(String privateGroupName) {
+        Chat toDelete = chatRepository.findChatByGroupName(privateGroupName);
+        if (toDelete != null && Objects.equals(toDelete.getOwnerId(), getSender().getId())) {
+            chatRepository.delete(toDelete);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean deletePrivateChat(long recipientId) {
+        Chat toDelete = chatRepository.getPrivateChat(getSender().getId(), recipientId);
+        if (toDelete != null) {
+            chatRepository.delete(toDelete);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
     /*
         ##Gibt alle Nachrichten aus, die vor Time geschrieben wurden##
 
